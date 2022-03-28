5b674c4204610383d5ed1007b45146f533769cfd
Pascal <77117642+Pascal273@users.noreply.github.com>
2022-03-28 18:23:03 +0200

    Nested routers installed

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested import routers
 from rest_framework.routers import DefaultRouter
 
 from api import views
@@ -7,6 +8,14 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'projects', views.ProjectViewSet)
 
+issue_router = routers.NestedDefaultRouter(
+    router,
+    r'projects',
+    lookup='project'
+)
+issue_router.register(r'issues', views.IssueViewSet, basename='project-issue')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(issue_router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import detail
 from rest_framework import viewsets, permissions, status
+from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
@@ -21,8 +22,14 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
 
 class IssueViewSet(viewsets.ModelViewSet):
-    queryset = Issue.objects.all()
+    queryset = Issue.objects.all().select_related('project')
     serializer_class = IssueSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-
+    def get_queryset(self, *args, **kwargs):
+        project_id = self.kwargs.get('project_pk')
+        try:
+            project = Project.objects.get(id=project_id)
+        except Project.DoesNotExist:
+            raise NotFound('A Project with that id does not exist')
+        return self.queryset.filter(project=project)
